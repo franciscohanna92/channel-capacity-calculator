519f0fade702237158e2e876ef1e2d21be85b3cb
franciscohanna92 <franciscohanna92@gmail.com>
2018-09-22 19:22:16 -0300

    simmetryc calculator ready

--- calculator.js
+++ calculator.js
@@ -1,4 +1,21 @@
-function calculateChannelCapacity(channelMatrix) {
+function calculateChannelCapacity(channelMatrix, arity) {
+	let result = {};
+	if(channelMatrix.length == 2) {
+		result = getBinaryProbx(arity);
+	}
+	if(channelMatrix.length == 3) {
+		result = getTernaryProbx(arity);
+	}
+	if(channelMatrix.length == 4) {
+		result = getCuaternaryProbx(arity);
+	}
+	
+
+	let HXY = calculateHXY(channelMatrix, result.probX, arity);
+	return result.maxHY - HXY
+}
+
+function getBinaryProbx(arity) {
 	let tmpProbX = [];
 	let probX = [];
 	let maxHY = 0;
@@ -12,7 +29,7 @@ function calculateChannelCapacity(channelMatrix) {
 					[i],
 					[j]
 				]
-				tmpHY = calculateEntropy(multiplyMatrices(channelMatrix, tmpProbX));
+				tmpHY = calculateEntropy(multiplyMatrices(channelMatrix, tmpProbX), arity);
 				if (tmpHY > maxHY) {
 					maxHY = tmpHY
 					probX = tmpProbX
@@ -20,27 +37,92 @@ function calculateChannelCapacity(channelMatrix) {
 			}
 		}
 	}
-
-	let HXY = calculateHXY(channelMatrix, probX);
-	return maxHY - HXY
+	return {
+		probX,
+		maxHY
+	}
 }
 
-function calculateHXY(channelMatrix, probX) {
+function getTernaryProbx(arity) {
+	let tmpProbX = [];
+	let probX = [];
+	let maxHY = 0;
+	let tmpHY = 0;
+
+	// Calculate X probabilties for maximum entropy in Y
+	for (let i = 0.1; i <= 1; i += 0.1) {
+		for (let j = 0.1; j <= 1; j += 0.1) {
+			for (let k = 0.1; k <= 1; k += 0.1) {
+				if (i + j + k == 1) {
+					tmpProbX = [
+						[i],
+						[j],
+						[k]
+					]
+					tmpHY = calculateEntropy(multiplyMatrices(channelMatrix, tmpProbX), arity);
+					if (tmpHY > maxHY) {
+						maxHY = tmpHY
+						probX = tmpProbX
+					}
+				}
+			}
+		}
+	}
+	return {
+		probX,
+		maxHY
+	}
+}
+
+function getCuaternaryProbx(arity) {
+	let tmpProbX = [];
+	let probX = [];
+	let maxHY = 0;
+	let tmpHY = 0;
+
+	// Calculate X probabilties for maximum entropy in Y
+	for (let i = 0.1; i <= 1; i += 0.1) {
+		for (let j = 0.1; j <= 1; j += 0.1) {
+			for (let k = 0.1; k <= 1; k += 0.1) {
+				for (let l = 0.1; l <= 1; l += 0.1) {
+					if (i + j + k + l == 1) {
+						tmpProbX = [
+							[i],
+							[j],
+							[k],
+							[l]
+						]
+						tmpHY = calculateEntropy(multiplyMatrices(channelMatrix, tmpProbX), arity);
+						if (tmpHY > maxHY) {
+							maxHY = tmpHY
+							probX = tmpProbX
+						}
+					}
+				}
+			}
+		}
+	}
+	return {
+		probX,
+		maxHY
+	}
+}
+
+function calculateHXY(channelMatrix, probX, arity) {
 	let HXY = 0;
 	for (let i = 0; i < probX.length; i++) {
-		HXY += probX[i][0] * calculateEntropy(channelMatrix[i]);
+		HXY += probX[i][0] * calculateEntropy(channelMatrix[i], arity);
 	}
 
 	return HXY;
 }
 
-function calculateEntropy(vector) {
+function calculateEntropy(vector, arity) {
 	let H = 0;
 	for (let i = 0; i < vector.length; i++) {
 		if (vector[i] != 0) {
-			H += vector[i] * Math.log2(1 / vector[i]);
+			H += vector[i] * Math.log(1 / vector[i]) / Math.log(arity);
 		}
-
 	}
 	return H;
 }
